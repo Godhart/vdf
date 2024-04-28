741a873fceae37e9e9d13c815dcae7d651c08201
Nikolay Gniteev <godhart@gmail.com>
2024-04-28 21:32:23 +0300

    [tests][test_read_the_doc] test input from file
    1st simple case passes
    Need to add corner cases and complex test

---
+++ tests/read_the_doc/test/markdown-01-simple/data.md
@@ -0,0 +1,22 @@
+---
+# Frontmatter part:
+
+title       : Versatile Description Format specification
+author      : Nikolay Gniteev (godhart@gmail.com)
+version     : 1.0.draft.0
+vdf         : "1.0"   # Используемая спецификация 
+
+---
+
+# First header
+
+test section
+
+---
+
+```vhdl
+%%vdf code
+
+---
+
+```
---
+++ tests/read_the_doc/test_read_the_doc.py
@@ -0,0 +1,56 @@
+import pytest
+import sys
+from pathlib import Path
+import yaml
+import json
+
+vdf_root_path = str((Path(__file__).absolute().parent.parent.parent).resolve())
+if vdf_root_path not in sys.path:
+    sys.path.insert(0, vdf_root_path)
+
+from tests.helpers import *
+from src.helpers import *
+from src.vdf.source_io import load_from_file
+from src.vdf.input import RawDocument
+from src.vdf.document import Document
+
+
+def parse_input_file(input_path):
+    result = {}
+    result['source']    = source = load_from_file(input_path)
+    result['preparsed'] = preparsed = RawDocument(source)
+    result['doc'] = Document(
+        source=preparsed.cells,
+        frontmatter=preparsed.frontmatter
+    )
+
+    return result
+
+
+@pytest.mark.parametrize("test_set", list_tests(__file__, ["test","gold"]))
+def test_parse_input_file(test_set):
+    """
+    Make sure that any to dict/list/scalar works properly
+    """
+    input_path, gold_path, output_path = init_test_paths(__file__, test_set)
+
+    value = parse_input_file(input_path.relative_to(os.getcwd()) /"data.md")
+
+    result = any_to_dict_list_scalar(value)
+    with open(output_path/"result.yaml", "w") as f:
+        yaml.safe_dump(result, f, allow_unicode=True)
+    with open(output_path/"result.json", "w") as f:
+        f.write(json.dumps(result, indent=2))
+
+    expected = [True, False][test_set[:4] == "err_"]
+
+    assert same_as_gold(gold_path, output_path) == expected
+
+
+if __name__ == "__main__":
+    """
+    NOTE: this branch is for debug purposes only
+    """
+    value = test_parse_input_file("1-simple_one")
+    result = any_to_dict_list_scalar(value)
+    a = 1
