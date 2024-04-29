9658ebad2519857a0677d4d648a7b91c903c4fa8
Nikolay Gniteev <godhart@gmail.com>
2024-04-29 12:38:18 +0300

    [src][processing] moar descriptions of what should be done when processing cell

--- src/vdf/processing.py
+++ src/vdf/processing.py
@@ -1,4 +1,4 @@
-from .cells import Cell
+from .cells import Cell, CodeCell, DocCell
 from .input import RawDocument
 from .document import Document
 from .literals import *
@@ -21,9 +21,14 @@ class VdfProcessor:
         # Take current state of document
         # Take cell
         # Detect tags in cell
+        #  - depends on whether it's code or doc cell
+        #  - make sure every tag is enabled
+        #  - look for cell name and check it's unique
         # Determine into which branch / flow cell should fall
         # Get context from branch
+        # Make temporary altered context by side tags (alter tags and vars)
         # Process cell according to context and tags
+        #   - convert data if there is #convert tag
         # Put cell into branch / flow
 
         # NOTE: it's possible that cell is in a raw text form
@@ -32,6 +37,18 @@ class VdfProcessor:
         if isinstance(cell, tuple):
             cell = RawDocument.parse_code_content(*cell)
 
+        # TODO: var's scopes
+
+        # Determine output file name
+        # Depends on
+        #   - subject
+        #   - lang
+        #   - kind
+        #   - target
+
+        # If default code file were defined already (implicitly by the first #code tag)
+        # make sure subject and/or kind or target is defined for code tags with other lang
+
     def initialize_doc(
         self,
         document    : Document
@@ -39,6 +56,12 @@ class VdfProcessor:
         """
         Initialize documents context
         """
+        # Run through all tags and run their's init code
+
+        # TODO: update tags like
+        # fetch
+        #
+
         if document.frontmatter is not None:
             pass
 
