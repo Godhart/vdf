996038b7200d0282399d52726cb27c3a071dbf61
Nikolay Gniteev <godhart@gmail.com>
2024-05-21 09:55:44 +0300

    [src] disabled jovyan import in root level __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,14 @@
-"""VDF Magic"""
-__version__ = '0.0.1'
+if False:
+    """VDF Magic"""
+    __version__ = '0.0.1'
 
-# as per
-# https://ipython.readthedocs.io/en/stable/config/custommagics.html#complete-example
+    # as per
+    # https://ipython.readthedocs.io/en/stable/config/custommagics.html#complete-example
 
-from .jovyan.vdf_magic import VdfMagic
+    from .jovyan.vdf_magic import VdfMagic
 
-_CONTEXT = {}
+    _CONTEXT = {}
 
-def load_ipython_extension(ipython):
-    magics = VdfMagic(ipython, _CONTEXT)
-    ipython.register_magics(magics)
+    def load_ipython_extension(ipython):
+        magics = VdfMagic(ipython, _CONTEXT)
+        ipython.register_magics(magics)
