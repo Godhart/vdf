d2b9cbcfa007f10e510dbdb77504b10927177c67
Nikolay Gniteev <godhart@gmail.com>
2024-05-21 07:56:07 +0300

    [project] moar to read for jupyter integration etc.

--- src/vdf/ju.py
+++ src/vdf/ju.py
@@ -6,3 +6,15 @@
 # https://discourse.jupyter.org/t/getting-syntax-highlighting-to-work-for-custom-cell-magic/11734/5
 
 # https://stackoverflow.com/questions/40518074/jupyter-get-arbitrary-notebook-cell-contents
+
+# https://ipython-books.github.io/
+#   - https://ipython-books.github.io/14-creating-an-ipython-extension-with-custom-magic-commands/
+
+# https://github.com/ipython/ipython/wiki/Extensions-Index
+#  - https://github.com/pythonnet/pythonnet
+#    https://github.com/den-run-ai/clrmagic/blob/master/README.md
+
+# https://flit.pypa.io/en/stable/
+
+
+# https://pypi.org/project/hdlConvertor/
